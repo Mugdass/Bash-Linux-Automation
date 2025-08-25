950f8565076400506e8248f501325e94a6893988
Matas Backevicius <84940755+Mugdass@users.noreply.github.com>
2025-08-25 11:47:39 -0700

    Update sample_test.sh

--- tests/sample_test.sh
+++ tests/sample_test.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+# Load the script to be tested
+. ./sample_script.sh
+
+# Sample unit test
+test_output_should_be_PASS() {
+  result=$(run_check)
+  assertEquals "PASS" "$result"
+}
+
+# Load shunit2
+. /usr/share/shunit2/shunit2
 
